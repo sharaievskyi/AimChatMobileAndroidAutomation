e90c77bbc7e0fba57baa43661e1199699f8e1ff6
sharaievskyi <d.sharaievskyi@aimprosoft.com>
2020-04-15 21:41:37 +0300

    Commit 18

--- src/test/java/com/aimprosoft/AuthorizationTestSuite.java
+++ src/test/java/com/aimprosoft/AuthorizationTestSuite.java
@@ -6,13 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = {
-        "src/test/resources/features/000_Precondition.feature",
         "src/test/resources/features/001_Authorization.feature",
         "src/test/resources/features/002_LeftDrawer.feature",
-        "src/test/resources/features/003_MessageHistory.feature"
-//        "src/test/resources/features/004_RightDrawer.feature",
-//        "src/test/resources/features/005_Toolbar.feature",
-//        "src/test/resources/features/006_MessageBar.feature"
+        "src/test/resources/features/003_MessageHistory.feature",
+        "src/test/resources/features/004_RightDrawer.feature",
+        "src/test/resources/features/005_Toolbar.feature",
+        "src/test/resources/features/006_MessageBar.feature"
         })
 
 public class AuthorizationTestSuite {
